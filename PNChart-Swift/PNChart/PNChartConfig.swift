5df11ac61c35936f3e2501761126239dae174c7a
kevinzhow <kevinchou.c@gmail.com>
2014-06-05 06:24:35 +0800

    Fix Color

--- PNChart-Swift/PNChart/PNChartConfig.swift
+++ PNChart-Swift/PNChart/PNChartConfig.swift
@@ -8,5 +8,5 @@
 
 import UIKit
 
-var PNGreenColor = UIColor(red: 77.0 / 255.0 , green: 196.0 / 255.0, blue: 122.0 / 255.0, alpha: 1.0)
-var PNGrey = UIColor(red: 186.0 / 255.0 , green: 186.0 / 255.0, blue: 186.0 / 255.0, alpha: 1.0)
+let PNGreenColor = UIColor(red: 77.0 / 255.0 , green: 196.0 / 255.0, blue: 122.0 / 255.0, alpha: 1.0)
+let PNGrey = UIColor(red: 186.0 / 255.0 , green: 186.0 / 255.0, blue: 186.0 / 255.0, alpha: 1.0)
